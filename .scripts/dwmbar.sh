b8b6a0dcda1215291868b7dce4b43b78460586da
Oleksii Kapula <kon2vpalto@gmail.com>
2024-06-04 22:02:41 +0300

    Make status the most beautiful one

--- .scripts/dwmbar.sh
+++ .scripts/dwmbar.sh
@@ -1,59 +1,62 @@
 #!/usr/bin/env sh
 
 battery() {
-	cap="$(cat /sys/class/power_supply/BAT0/capacity)"
-	stat="$(cat /sys/class/power_supply/BAT0/status)"
+  cap="$(cat /sys/class/power_supply/BAT0/capacity)"
+  stat="$(cat /sys/class/power_supply/BAT0/status)"
 
-	printf "%s %s%%" "BAT0" "${cap}"
+  icon="󰂃"
+  [[ "${stat}" = "Charging" ]] && icon="󰂄"
+
+  printf "%s %s%%" "${icon}" "${cap}"
 }
 
 brightness() {
-	brt="$(calc "100 * $(brightnessctl get) / $(brightnessctl max)")"
-	brt="$(printf "%.*f" 0 "${brt:2:-1}")"
+  brt="$(calc "100 * $(brightnessctl get) / $(brightnessctl max)")"
+  brt="$(printf "%.*f" 0 "${brt:2:-1}")"
 
-	printf "%s %s%%" "BRT" "${brt}"
+  printf "%s %s%%" "󱠂" "${brt}"
 }
 
 datetime() {
-	date="$(date '+%b %d %a')"
-	time="$(date '+%R')"
+  date="$(date '+%b %d %a')"
+  time="$(date '+%R')"
 
-	printf "%s %s" "${date}" "${time}"
+  printf "%s %s" "${date}" "${time}"
 }
 
 wlan() {
-	case "$(cat /sys/class/net/wl*/operstate 2> /dev/null)" in
-		up)
-			stat="Up"
-			;;
-		down)
-			stat="Dn"
-			;;
-	esac
+  case "$(cat /sys/class/net/wl*/operstate 2> /dev/null)" in
+    up)
+      stat="󱚽 Up"
+      ;;
+    down)
+      stat="󱚼 Dn"
+      ;;
+  esac
 
-	printf "%s %s" "WLAN" "${stat}"
+  printf "%s" "${stat}"
 }
 
 layout() {
-	key="$(xset -q | grep "LED" | awk "{ print \$10 }")"
+  key="$(xset -q | grep "LED" | awk "{ print \$10 }")"
 
-	label="us"
-	[[ "${key}" = "00001000" ]] && label="ua"
+  label="us"
+  [[ "${key}" = "00001000" ]] && label="ua"
 
-	printf "%s %s" "KLY" "${label}"
+  printf "${prp}%s %s" "" "${label}"
 }
 
 volume() {
-	mute="$(pactl get-sink-mute @DEFAULT_SINK@)"
-	vol="$(pactl get-sink-volume @DEFAULT_SINK@ \
-		| grep "Volume:" | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')"
+  mute="$(pactl get-sink-mute @DEFAULT_SINK@)"
+  vol="$(pactl get-sink-volume @DEFAULT_SINK@ \
+    | grep "Volume:" | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')"
 
-	label="VOL"
-	if [[ $mute = "Mute: yes" ]]; then
-		label="V01"
-	fi
+  icon="󱄡"
+  if [[ $mute = "Mute: yes" ]]; then
+   icon="󰸈"
+  fi
 
-	printf "%s %s%%" "${label}" "${vol}"
+  printf "%s %s%%" "${icon}" "${vol}"
 }
 
-printf "%s" " $(layout) / $(volume) / $(brightness) / $(battery) / $(wlan) / $(datetime) "
+printf "%s" " $(layout) 󰿟 $(volume) 󰿟 $(brightness) 󰿟 $(battery) 󰿟 $(wlan) 󰿟 $(datetime) "
