d6fc14486f178458b4301fffb47ec36df0182c76
Oleksii Kapula <kon2vpalto@gmail.com>
2024-06-17 15:36:21 +0300

    Get rid of unnecessary modules

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -60,12 +60,12 @@
 
        :editor
        (evil +everywhere) ; come to the dark side, we have cookies
-       file-templates       ; auto-snippets for empty files
-       fold                 ; (nigh) universal code folding
+       ;;file-templates     ; auto-snippets for empty files
+       ;;fold               ; (nigh) universal code folding
        ;;(format +onsave)   ; automated prettiness
        ;;god                ; run Emacs commands without modifier keys
        ;;lispy              ; vim for lisp, for people who don't like vim
-       multiple-cursors   ; editing in many places at once
+       ;;multiple-cursors   ; editing in many places at once
        ;;objed              ; text object editing for the innocent
        ;;parinfer           ; turn lisp into python, sort of
        ;;rotate-text        ; cycle region at point between text candidates
@@ -116,7 +116,7 @@
 
        :os
        (:if (featurep :system 'macos) macos) ; improve compatibility with macOS
-       (tty +osc)                            ; improve the terminal Emacs experience
+       ;;(tty +osc)                            ; improve the terminal Emacs experience
 
        :lang
        ;;agda              ; types of types of types of types...
@@ -155,7 +155,7 @@
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
        ;;lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
+       ;;markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
