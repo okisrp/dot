fd507667eb4311b12f075220dfa60cc94c2709b8
Oleksii Kapula <kon2vpalto@gmail.com>
2024-07-16 20:57:09 +0300

    Only polish keys allowed

--- .config/nvim/lua/local/keymap.lua
+++ .config/nvim/lua/local/keymap.lua
@@ -1,14 +1,18 @@
-vim.keymap.set({ "n", "i" }, "<M-q>", vim.cmd.xall)
+local function map(mode, lhs, rhs, opts)
+	local opts, t = opts or { noremap = true }, {}
+	mode:gsub(".", function(c) table.insert(t, c) end)
+	vim.keymap.set(t, lhs, rhs, opts)
+end
 
-vim.keymap.set("n", "<M-s>", vim.cmd.write)
+map("nvi", "<C-q>", vim.cmd.xall)
+map("nvi", "<C-s>", vim.cmd.write)
 
-vim.keymap.set("n", "<M-e>", vim.cmd.bdelete)
+map("n", "<M-e>", vim.cmd.bdelete)
+map("n", "<M-k>", vim.cmd.bprevious)
+map("n", "<M-j>", vim.cmd.bnext)
 
-vim.keymap.set("n", "<M-k>", vim.cmd.bprevious)
-vim.keymap.set("n", "<M-j>", vim.cmd.bnext)
+map("c", "<M-k>", "<Up>")
+map("c", "<M-j>", "<Down>")
 
-vim.keymap.set("c", "<M-k>", "<Up>")
-vim.keymap.set("c", "<M-j>", "<Down>")
-
-vim.keymap.set("v", "<", "<gv")
-vim.keymap.set("v", ">", ">gv")
+map("v", "<", "<gv")
+map("v", ">", ">gv")
