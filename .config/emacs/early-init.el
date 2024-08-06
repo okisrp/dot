83bf3c0ac0a0d0f155ea299000dc8c5d30f17504
Oleksii Kapula <kon2vpalto@gmail.com>
2024-08-06 22:29:57 +0300

    Make Emacs configuration with comments

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,24 +1,29 @@
 ;;; early-init.el --- Early configuration. -*- lexical-binding: t -*-
 
+;; Increase startup speed.
 (setq gc-cons-threshold most-positive-fixnum)
 (add-hook 'emacs-startup-hook
 		  (lambda (&rest _)
 			(setq gc-cons-threshold (expt 2 23))))
 
+;; Prevent `package.el' from loading packages prior to their init-file
+;; loading.
 (setq package-enable-at-startup nil)
 
+;; Put native compilation errors and warnings to separate buffer that
+;; won't pop up all of sudden.
 (when (native-comp-available-p)
   (setq native-comp-async-report-warnings-errors 'silent))
 
-(startup-redirect-eln-cache (expand-file-name "tmp/eln-cache/" user-emacs-directory))
-
+;; Treat a frame by pixels not by its char width and height.
 (customize-set-variable 'frame-resize-pixelwise t)
 
-(add-to-list 'default-frame-alist '(vertical-scroll-bars . nil))
-(add-to-list 'default-frame-alist '(horizontal-scroll-bars . nil))
-
-(add-to-list 'default-frame-alist '(menu-bar-lines . nil))
-(add-to-list 'default-frame-alist '(tool-bar-lines nil))
+;; Remove tool, menu and scroll bars from all frames.
+(dolist (property '((vertical-scroll-bars . nil)
+					(horizontal-scroll-bars . nil)
+					(menu-bar-lines . nil)
+					(tool-bar-lines . nil)))
+  (add-to-list 'default-frame-alist `(,(car property) . ,(cdr property))))
 
 ;; NOTE: If you're using `DOOM-emacs' framework that put these lines
 ;; at the begining of `$DOOMDIR/init.el' file.
--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,17 +1,24 @@
 ;;; init.el --- Main configuration. -*- lexical-binding: t -*-
 
-(setq inhibit-startup-screen t)
+;; Stop displaying startup screen.
+(setq inhibit-startup-message t
+      inhibit-splash-screen t
+      inhibit-startup-screen t)
 
-(defun display-startup-echo-area-message (&rest _)
-  (let ((time (format "%.1f" (float-time (time-subtract after-init-time before-init-time)))))
-    (message "Loaded in %s seconds with %d garbage collection." time gcs-done)))
+;; Stop providing information in startup echo area message.
+(put 'inhibit-startup-echo-area-message 'saved-value t)
+(setq inhibit-startup-echo-area-message (user-login-name))
 
-(setq idle-update-delay 1.0)
+;; Get the latest version of `straight.el', but it's going to be less
+;; stable.
+(setq straight-repository-branch "develop")
 
-(setq straight-repository-branch "develop"
-      straight-base-dir (expand-file-name "tmp/" user-emacs-directory)
-	  straight-check-for-modifications nil)
+;; Configure an alternative way for `straight.el' to check for
+;; modifications made to package source code in order to speed up
+;; starup time.
+(setq straight-check-for-modifications nil)
 
+;; Next-generation, purely functional package manager.
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name
@@ -28,11 +35,19 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-(setq straight-vc-git-default-clone-depth 1
-      straight-use-package-by-default t)
+;; Reduce the size of repositories.
+(setq straight-vc-git-default-clone-depth 1)
 
+;; If you use `use-package', then this makes each 'use-package' form
+;; also invoke `straight.el' to install the packages, unless otherwise
+;; specified.
+(setq straight-use-package-by-default t)
+
+;; To use `use-package', first install it with `straight.el'.
 (straight-use-package 'use-package)
 
+;; The community-driven pastel theme that aims to be the middle ground
+;; between low and high contrast themes.
 (use-package catppuccin-theme
   :straight
   (catppuccin-theme :host github :repo "catppuccin/emacs")
@@ -41,13 +56,16 @@
   (catppuccin-highlight-matches t)
   (catppuccin-italic-comments t)
   :config
+  ;; Change `catppuccin-theme' when inside of a terminal.
   (unless (window-system)
-	(setq catppuccin-flavor 'mocha))
+    (setq catppuccin-flavor 'mocha))
   (load-theme 'catppuccin 'no-confirm)
   (defface catppuccin-theme-visible-bell
     `((t :background ,(catppuccin-get-color 'maroon)
-		 :foreground ,(catppuccin-get-color 'base))) "")
+		 :foreground ,(catppuccin-get-color 'base)))
+    "Define face for `catppuccin-theme' visible bell implementation.")
   (defun catppuccin-theme-visible-bell-fn (&rest _)
+    "Implement the logic for visible bell function. "
     (let* ((face (if (facep 'mode-line-active)
                      'mode-line-active
                    'mode-line))
@@ -59,16 +77,22 @@
 						(with-current-buffer buf
                           (face-remap-remove-relative cookie)
                           (force-mode-line-update))))))
+  ;; Enable visible bell.
   (setq ring-bell-function 'catppuccin-theme-visible-bell-fn
 		visible-bell t)
+  ;; Highlight line at point.
   (global-hl-line-mode 1)
+  ;; Disable cursor blinking.
   (blink-cursor-mode 0))
 
+;; Construct unique buffer names for files with the same base name.
+(setq uniquify-buffer-name-style 'forward)
+
+;; Customize mode line (I don't know what is going on here).
 (setq-default mode-line-format
 			  '("%e"
 				(:propertize " " display (raise +0.1))
 				(:propertize " " display (raise -0.1))
-				(:propertize "λ " face font-lock-comment-face)
 				mode-line-frame-identification
 				mode-line-buffer-identification
 				(:eval (propertize
@@ -78,133 +102,89 @@
 									 ,(+ 2 (string-width "%4l:3%c")))))))
 				(:propertize "%4l:%c" face mode-line-buffer-id)))
 
+;; Set default font family for `default', that's actually more than
+;; enough, and `fixed-pitch' faces.
 (let ((font-family "Victor Mono"))
   (when (member font-family (font-family-list))
     (dolist (face '(default fixed-pitch))
       (set-face-attribute face nil :font (font-spec :family font-family :size 20 :weight 'medium)))))
 
-(setq-default tab-width 4
-			  indent-tabs-mode t)
+;; Do not create backups and lock files.
+(setq make-backup-files nil
+      create-lockfiles nil)
 
-(add-hook 'write-file-functions 'delete-trailing-whitespace)
-
-(dolist (hook '(prog-mode-hook conf-mode-hook))
-  (add-hook hook 'electric-pair-local-mode))
-
-(setq uniquify-buffer-name-style 'forward)
-
-(setq switch-to-prev-buffer-skip-regexp "\*[^*]+\*")
-
-(setq display-buffer-alist
-      '(("\\*[Hh]elp*" (display-buffer-same-window))
-		("\\*Occur\\*" (display-buffer-same-window))
-		("\\*Shell Command Output\\*" (display-buffer-same-window))))
-
-(setq Man-notify-method 'pushy)
-
-(let* ((directory (expand-file-name "tmp/tramp/" user-emacs-directory))
-	   (_ (or (file-directory-p directory) (make-directory directory 'parents))))
-  (setq tramp-backup-directory-alist `(("." . ,(expand-file-name "backups/" directory)))
-		tramp-persistency-file-name (expand-file-name "tramp.el" directory)))
-
-(setq backup-directory-alist `(("." . ,(expand-file-name "tmp/backups/" user-emacs-directory)))
-      make-backup-file t)
-
-(let ((directory (expand-file-name "tmp/lock-files/" user-emacs-directory)))
-  (unless (file-directory-p directory)
-    (make-directory directory 'parents))
-  (setq lock-file-name-transforms `((".*" ,directory t))
-		create-lockfiles t))
-
-(let ((directory (expand-file-name "tmp/auto-saves/" user-emacs-directory)))
-  (unless (file-directory-p directory)
-    (make-directory directory 'parents))
-  (setq auto-save-list-file-prefix (expand-file-name "sessions/" directory)
-		auto-save-file-name-transforms `((".*" ,directory t))))
-
-(setq recentf-save-file (expand-file-name "tmp/recentf.el" user-emacs-directory)
-	  recentf-exclude '("emacs/tmp/")
-	  recentf-max-saved-items 20)
+;; Get access to recent visited files.
 (recentf-mode 1)
 
-(setq savehist-file (expand-file-name "tmp/history.el" user-emacs-directory)
-      history-length 20)
+;; Save minibuffer history.
 (savehist-mode 1)
 
-(setq save-place-file (expand-file-name "tmp/places.el" user-emacs-directory))
+;; Save cursor position of each file.
 (save-place-mode 1)
 
-(setq transient-history-file (expand-file-name "tmp/transient/history.el" user-emacs-directory)
-      transient-levels-file (expand-file-name "tmp/transient/levels.el" user-emacs-directory)
-      transient-values-file (expand-file-name "tmp/transient/values.el" user-emacs-directory))
-
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(when (file-exists-p custom-file)
-  (load custom-file nil 'nomessage))
-
-(let ((directory (or (getenv "XDG_DATA_HOME")
-					 (expand-file-name ".local/share/" (getenv "HOME")))))
-  (setq trash-directory (expand-file-name "Trash/files" directory)
-		delete-by-moving-to-trash t))
-
-(setq bookmark-default-file (expand-file-name "tmp/bookmarks.el" user-emacs-directory)
-	  bookmark-save-flag t)
-
-(setq dired-kill-when-opening-new-dired-buffer t)
-
+;; Update a buffer when it has been changed.
 (setq global-auto-revert-non-file-buffers t)
 (global-auto-revert-mode 1)
 
-(setq backward-delete-char-untabify-method 'hungry)
+;; Don't pile up a heap of `dired' buffers.
+(setq dired-kill-when-opening-new-dired-buffer t)
 
-(delete-selection-mode 1)
+;; Use shorter answers `y-or-n-p'.
+(setq use-short-answers t)
 
-(setq scroll-step 1
-      scroll-margin 2)
+;; Delete file to trash bin.
+(setq trash-directory (expand-file-name ".local/share/Trash/files/" (getenv "HOME"))
+	  delete-by-moving-to-trash t)
 
+;; Ignore case whether it's lower or upper one.
+(setq completion-ignore-case t
+      read-buffer-completion-ignore-case t
+      read-file-name-completion-ignore-case t)
+
+;; Scroll by one line without twitching.
+(setq scroll-conservatively (expt 2 7))
+
+;; Make scrolling normal.
 (setq mouse-wheel-progressive-speed nil
       mouse-wheel-follow-mouse t
       mouse-wheel-scroll-amount '(2 ((shift) . hscroll))
       mouse-wheel-scroll-amount-horizontal 2)
 
-(setq use-short-answers t)
+;; Trim extra lines and spaces when saving a file.
+(add-hook 'write-file-functions 'delete-trailing-whitespace)
 
-(bind-key "<escape>" 'keyboard-escape-quit)
+;; Delete tab instead of space when it's a tab.
+(setq backward-delete-char-untabify-method 'hungry)
 
-(dolist (key '("C-x C-u" "C-z"))
-  (unbind-key key))
+;; Delete selected text when start to type.
+(delete-selection-mode 1)
 
-(bind-keys ("C-z" . undo)
-		   ("C-S-z" . undo-redo))
+;; Enable tabs instead of space and set their size.
+(setq-default tab-width 4
+			  indent-tabs-mode t)
 
-(bind-keys ("M-p" . previous-buffer)
-		   ("M-n" . next-buffer))
+;; Enable auto-pairing in programming and configuration modes.
+(dolist (hook '(prog-mode-hook conf-mode-hook))
+  (add-hook hook 'electric-pair-local-mode))
 
-(bind-keys :prefix "M-SPC"
-		   :prefix-map M-SPC-prefix-map
-		   ("k" . kill-current-buffer)
-		   ("i" . ibuffer)
-		   ("r" . recentf-open-files))
-
+;; Replace `help' buffer for `helpful'.
 (use-package helpful
   :bind
   ([remap describe-variable] . helpful-variable)
   ([remap describe-function] . helpful-function)
-  ([remap describe-key] . helpful-key)
   ([remap describe-command] . helpful-command)
+  ([remap describe-key] . helpful-key)
   ("C-h c" . helpful-callable)
   ("C-h d" . helpful-at-point))
 
+;; Tell which key should be pressed.
 (use-package which-key
   :custom
   (which-key-min-column-description-width 20)
   :config
   (which-key-mode 1))
 
-(setq completion-ignore-case t
-      read-buffer-completion-ignore-case t
-      read-file-name-completion-ignore-case t)
-
+;; Make vertical completion.
 (use-package vertico
   :bind (:map vertico-map
 			  ("M-p" . vertico-previous)
@@ -217,16 +197,19 @@
   :init
   (vertico-mode 1))
 
+;; Make no order.
 (use-package orderless
   :custom
   (completion-styles '(orderless basic))
   (completion-category-defaults nil)
   (completion-category-overrides '((file (styles partial-completion)))))
 
+;; Add description for vertical completion.
 (use-package marginalia
   :init
   (marginalia-mode 1))
 
+;; Set second input method.
 (setq default-input-method "ukrainian-computer")
 
 ;; NOTE: `google-translate' package somehow messes up with
@@ -237,13 +220,16 @@
 	  (deactivate-input-method)
 	(set-input-method (or default-input-method "ukrainian-computer"))))
 
+;; Invoke `toggle-im' instead of `toggle-input-method'.
 (bind-key [remap toggle-input-method] 'toggle-im)
 
+;; Use translator when learning another language.
 (use-package google-translate
   :init
   (setq google-translate-preferable-input-methods-alist
         '((nil) ("ukrainian-computer" . ("uk" "ru"))))
   :config
+  ;; Make `google-translate' listen button normal sized.
   (set-face-attribute
    'google-translate-listen-button-face nil :height 1.0)
   (setq google-translate-listen-button-label "[Listen]"
@@ -257,12 +243,14 @@
         google-translate-display-translation-phonetic nil
         google-translate-input-method-auto-toggling t)
   (defun google-translate-clipboard (&rest _)
+	"Translate text from clipboard using `google-translate' package."
     (interactive)
     (let ((source google-translate-default-source-language)
           (target google-translate-default-target-language)
           (output google-translate-output-destination)
           (text (gui-get-selection 'CLIPBOARD 'TEXT)))
       (google-translate-translate source target text output)))
+  ;; Make leader key for `google-translate' functions.
   (bind-keys :prefix "M-o"
 			 :prefix-map M-o-prefix-map
 			 ("q" . google-translate-query-translate)
@@ -270,3 +258,39 @@
 			 ("s" . google-translate-smooth-translate)
 			 ("w" . google-translate-at-point)
 			 ("c" . google-translate-clipboard)))
+
+;; Make ESC key behave as it should.
+(bind-key "<escape>" 'keyboard-escape-quit)
+
+;; Don't want to hit these keys by accident.
+(dolist (key '("C-x C-u" "C-z"))
+  (unbind-key key))
+
+;; Make more intuitive `undo' and `undo-redo' keybindings.
+(bind-keys ("C-z" . undo)
+		   ("C-S-z" . undo-redo))
+
+;; Ignore buffers that start with asterisk when switching between them.
+(setq switch-to-prev-buffer-skip-regexp "\*[^*]+\*")
+
+;; Make moving around buffer more faster.
+(bind-keys ("M-p" . previous-buffer)
+		   ("M-n" . next-buffer))
+
+(use-package consult)
+
+;; Make something similar to leader key.
+(bind-keys :prefix "M-SPC"
+		   :prefix-map M-SPC-prefix-map
+		   ("k" . kill-current-buffer)
+		   ("i" . ibuffer)
+		   ("r" . recentf-open-files)
+		   ("l" . consult-line)
+		   ("d" . consult-ripgrep)
+		   ("o" . consult-line-multi)
+		   ("f" . consult-find))
+
+;; Do not use user interface to configure, but if you do then:
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file 'noerror 'nomessage))
