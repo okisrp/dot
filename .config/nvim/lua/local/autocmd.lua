379f3370d23677981fd5015644eb74f5118a0886
Oleksii Kapula <kon2vpalto@gmail.com>
2024-08-10 16:48:33 +0300

    Re-write autocmds with nvim_create_autocmd

--- .config/nvim/lua/local/autocmd.lua
+++ .config/nvim/lua/local/autocmd.lua
@@ -1,9 +1,14 @@
-vim.cmd [[
-	au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif
-]]
+-- Save cursor position on exit.
+vim.api.nvim_create_autocmd("BufReadPost", {
+	pattern = "*",
+	callback = function()
+		vim.cmd [[ if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif ]]
+	end,
+})
 
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-	pattern = { "*" },
+-- Remove extra whitespaces at the end of lines.
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
 	callback = function()
 		local save_cursor = vim.fn.getpos(".")
 		pcall(function () vim.cmd [[%s/\s\+$//e]] end)
@@ -11,13 +16,17 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	end,
 })
 
+-- Highlight yanked text.
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("HighlightYank", { clear = true }),
+	pattern = "*",
 	callback = function()
-		vim.highlight.on_yank({ timeout = 150 })
+		vim.highlight.on_yank({ higroup = "IncSearch",  timeout = 150 })
 	end,
 })
 
-vim.cmd [[
-	augroup HelpBuffer | au! filetype help only | augroup END
-]]
+-- Open help buffer as whole window.
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "help",
+	group = vim.api.nvim_create_augroup("HelpBuf", { clear = true }),
+	command = "only",
+})
